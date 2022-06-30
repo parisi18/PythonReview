3d7111ece75369612addaba691085dc7f8ed5814
ParisiBR <66031419+parisi18@users.noreply.github.com>
2022-06-29 21:53:06 -0300

    Delete desafio_2.py

--- desafio_2.py
+++
@@ -1,8 +0,0 @@
-print('===DESAFIO 2 ===')
-
-#Dia do nascimento
-dia = input('Dia: ')
-mes = input('Mes: ')
-ano = input('Ano: ')
-
-print('Você nasceu no dia: ', dia, ' de ', mes, ' de ', ano, '. Correto?')
