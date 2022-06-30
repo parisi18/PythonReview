e7b49d4f4113941a7d59da57e23f72fdc76c365e
Rafael dos Santos Parisi <rafael.parisi@autaza.com>
2022-06-29 21:51:57 -0300

    Initial challenge

---
+++ desafio_2.py
@@ -0,0 +1,8 @@
+print('===DESAFIO 2 ===')
+
+#Dia do nascimento
+dia = input('Dia: ')
+mes = input('Mes: ')
+ano = input('Ano: ')
+
+print('Você nasceu no dia: ', dia, ' de ', mes, ' de ', ano, '. Correto?')
