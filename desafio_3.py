6e58d150604fcfb1d4bcb3266f82eafbd98f94fe
ParisiBR <66031419+parisi18@users.noreply.github.com>
2022-06-29 21:42:57 -0300

    Delete desafio_3.py

--- desafio_3.py
+++
@@ -1,7 +0,0 @@
-print('===DESAFIO 3 ===')
-
-num_a = int(input('Digite um número: '))
-num_b = int(input('Digite outro número: '))
-num_c = num_a + num_b
-
-print('A soma entre {} e {} é: {}'.format(num_a, num_b, num_c))
